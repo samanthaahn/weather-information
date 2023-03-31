2fd8f06f73a0ca3eb25a1b8b55cd07440f2162a7
Samantha Ahn <samanthaahn@Samanthas-MBP.attlocal.net>
2023-03-30 17:07:53 -0700

    added local storage to my js

--- assets/script.js
+++ assets/script.js
@@ -1,27 +1,43 @@
 var input = document.querySelector(".input");
 var submitBtn = document.querySelector(".submit");
 
-
+// this is for the weather icons: https://openweathermap.org/img/w/10d.png
 
 
 
 //This is my event listener for the current day
 submitBtn.addEventListener("click", function () {
   fetch(
-    "https://api.openweathermap.org/data/2.5/weather?q=" + input.value +"&appid=ce895f0400d0fe16be29264e2d060cb6").then(function (response) {
+    "https://api.openweathermap.org/data/2.5/weather?q=" + input.value +"&appid=ce895f0400d0fe16be29264e2d060cb6"
+    )
+    .then(function (response) {
+      localFunction();
     return response.json();
-  }
-  )}.then(function (data) {
+  })
+.then(function (data) {
     console.log(data);
     var currentDay = document.createElement('div');
     currentDay.classList.add('card');
-    currentDay.innerHTML = "";
+    currentDay.innerHTML = "<div class='card-body'><h5 class='card-title'>" +
+    "Date:" +
+    data.dt +
+    "</h5><h6 class='card-subtitle mb-2 text-muted'>" +
+    "Temperature:" +
+    data.main.temp +
+    "<h6 class='card-subtitle mb-2 text-muted'>" +
+    "Wind Speed" +
+    data.wind.speed +
+    "<h6 class='card-subtitle mb-2 text-muted'>" +
+    "Humidity:" +
+    data.main.humidity +
+    "<h6></div>";
+
     var currentDayCard = document.querySelector('.currentday');
     currentDayCard.append(currentDay);
-
   }
-  )
-  );
+  )}
+);
+
 // This is my event listener for my 5-day forcast
 submitBtn.addEventListener("click", function () {
   fetch(
@@ -30,8 +46,9 @@ submitBtn.addEventListener("click", function () {
       "&appid=ce895f0400d0fe16be29264e2d060cb6"
   )
     .then(function (response) {
+      localFunction();
       return response.json();
-      // localStorage.setItem('searchedCity', JSON.stringify(input))
+      
     })
     .then(function (data) {
       console.log(data);
@@ -63,30 +80,16 @@ submitBtn.addEventListener("click", function () {
     });
 });
 
+function localFunction () {
+var searchedCity = JSON.parse(localStorage.getItem('searchedCity'));
 
+      if (!searchedCity){
+      localStorage.setItem('searchedCity', JSON.stringify([input.value]));
+      }else {
+      searchedCity.push(input.value);  
+      localStorage.setItem('searchedCity', JSON.stringify(searchedCity));
+      }
 
+    
+    }
 //5 day forcast: Date, icon, weather, conditions, temperature, wind StereoPannerNode, and humidity
-// if (i < 9) {
-//   console.log("we made it!");
-//   var cardInfoCenter = document.createElement("div");
-//   cardInfoCenter.classList.add("card");
-//   cardInfoCenter.innerHTML =
-//     "<div class='card-center'><h5 class='card-title'>" +
-//     "Date:" +
-//     data.list[i].dt_txt +
-//     "</h5><h6 class='card-subtitle mb-2 text-muted'>" +
-//     "Temperature:" +
-//     data.list[i].main.temp +
-//     "<h6 class='card-subtitle mb-2 text-muted'>" +
-//     "Wind Speed" +
-//     data.list[i].wind.speed +
-//     "<h6 class='card-subtitle mb-2 text-muted'>" +
-//     "Humidity:" +
-//     data.list[i].main.humidity +
-//     "<h6 class='card-subtitle mb-2 text-muted'>" +
-//     data.list[i].main.humidity +
-//     "<h6></div>";
-//   var cardInfo = document.querySelector(".card-center");
-//   cardInfo.append(cardInfoCenter);
-//   console.log(cardInfo);
-//   console.log(cardInfoCenter);
